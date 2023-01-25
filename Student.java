0665e9cd9b5ca9448aef73b17f08962672f0ed62
MD.RONY <123396538+RONY-1@users.noreply.github.com>
2023-01-25 22:14:23 +0600

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    public static void main( String[] args){
+
+        System.out.println("Student name: Md.Rony");
+        System.out.println("University: Daffodil International University");
+        System.out.println("Department: Computer science & Engineering");
+        System.out.println("Student Id: 221-15-4967");
+        System.out.println("Batch Code : 221");
+        System.out.println("Section: V");
+        System.out.println("Student identity: CLASS REPRESENTATIVE-CR");
+        System.out.println("Course : Object Oriented Programming");
+        System.out.println("Teacher: Nasima Islam Bithi");
+                System.out.println("Personal information About Rony");
+        System.out.println("Hometown : Tangail");
+        System.out.println("Email: rony15-4967@diu.edu.bd" );
+        System.out.println("Mobile no: 01727371436");
+        System.out.print ("Thank you");
+
+
+    }
+}
