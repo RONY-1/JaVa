786cc8b550286f8701c4220704cef37c6d9da35f
MD.RONY <123396538+RONY-1@users.noreply.github.com>
2023-01-26 22:38:19 +0600

    Add files via upload

---
+++ SkapeOperation.java
@@ -0,0 +1,33 @@
+public class SkapeOperation {
+    public static void main(String[] src) {
+
+        System.out.println("Name:Md.Rony\nMobile:0119876543\nBSc in Computer Science & Engineering\n"); //[NEW LINE \n]
+
+
+        System.out.println("1 \t 2 \t 3 \t 4 \t 5 ");     //[NEW TAB \t]
+        System.out.println("6 \t 7 \t 8 \t 9 \t 10\n");     //[NEW TAB \t]
+
+
+        System.out.println("\"I LOVE OBJECT ORIENTED PROGRAMMING WORK (-_-)\"");    // [PRINTING DOUBLE COT \"  \"]
+
+
+    }
+}
+
+
+
+
+
+
+
+
+/*[Ak line ae onek golo line banano jai just '\n' aita likhae aita Skape Sequence key ]
+            Aro kiso skape sequence asae for example ----
+
+                                  \b= Back space
+                                  \t= Tab
+                                  \n= New line
+                                  \"= Print double cotetion [ ("\"Rony\"") ]= "Rony"
+                                  \'= printing invitade single cote
+                                  \\= printing slash
+             */
