922e4d1dd18314c4480629267585639c6d0833fd
MD.RONY <123396538+RONY-1@users.noreply.github.com>
2023-02-13 01:20:41 +0600

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,23 @@
+public class employee {
+    int empid;
+    String empname;
+    employee (int id ,String name){
+        this.empid=id;
+        this.empname=name;
+    }
+    void info(){
+
+    System.out.println("id:"+empid+"name:"+empname);
+
+    }
+
+
+    public static void main (String [] args){
+        employee obj1= new employee( 221-15-4967 ,  "Rony");
+        employee obj2= new employee(221-15-5919 , "Asif");
+        obj1.info();
+        obj2.info();
+
+
+    }
+}
