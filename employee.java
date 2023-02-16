27407bac1d166d15aedadf6692814268284593e3
MD.RONY <123396538+RONY-1@users.noreply.github.com>
2023-02-16 22:35:18 +0600

    Add files via upload

--- employee.java
+++ employee.java
@@ -13,20 +13,11 @@ public class employee {
 
 
     public static void main (String [] args){
-        employee obj1= new employee( 221-15-4967 ,  "Rony");
-        employee obj2= new employee(221-15-5919 , "Asif");
+        employee obj1= new employee( 4967 ,    "Rony");
+        employee obj2= new employee(5919 ,    "Asif");
         obj1.info();
         obj2.info();
 
 
     }
 }
-
-
-
-___________
-OUTPUT:
-__________
-id:4967name:Rony
-id:5919name:Asif
-_________________
